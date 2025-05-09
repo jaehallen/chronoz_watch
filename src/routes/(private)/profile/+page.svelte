49bdb0034b117f99b7579f2ec6bf431c126888bd
PrawnsJas <156972633+PrawnsJas@users.noreply.github.com>
2025-05-09 09:59:26 +0800

    Update +page.svelte Profile

--- src/routes/(private)/profile/+page.svelte
+++ src/routes/(private)/profile/+page.svelte
@@ -1,1 +1,11 @@
-<h1>PROFILE</h1>
+<script lang="ts">
+  import type { PageData } from './$types';
+
+</script>
+
+<main class="container">
+  <section class="section">
+    
+  </section>
+</main>
+
